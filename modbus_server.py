6b508f3a88de96eb02fb97eb09a8f728c665d215
Thea168-create <thea.t@tapeffect.asia>
2024-12-10 22:43:44 +0700

    Update modbus_server.py

--- modbus_server.py
+++ modbus_server.py
@@ -1,9 +1,7 @@
 from pymodbus.server.sync import StartTcpServer
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-from pymodbus.transaction import ModbusTcpFramer
 import logging
-import socket
 
 # Enable logging to display messages in the terminal
 logging.basicConfig()
@@ -17,20 +15,6 @@ store = ModbusSlaveContext(
 )
 
 context = ModbusServerContext(slaves=store, single=True)
-
-# Function to log when data is read or written
-def log_request(request):
-    # Log client connection info (client's IP address and port)
-    client_ip = request.client
-    client_port = request.client_port
-    print(f"Connection from {client_ip}:{client_port} - Request received: {request}")
-    return request
-
-# Override the default read/write functions to include logging
-context.read_holding_registers = log_request
-context.write_registers = log_request
-context.read_coils = log_request
-context.write_coils = log_request
 
 # Start the Modbus TCP server on port 1234
 if __name__ == "__main__":
